26fc37d55332a24a04ad7f33c550e28423cf5975
JTabaries <59892925+JTabaries@users.noreply.github.com>
2024-10-18 16:39:29 +0200

    modified:   app.js

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-import {watch} from 'node:fs/promises';
+import {readdir, stat} from 'node:fs/promises';
 
-const watcher = watch('./');
-for await(const event of watcher){
-    console.log(event);
+const files =await readdir('./',{withFileTypes:true});
+for (const file of files){
+    const parts=[
+        file.isDirectory() ? 'D' : 'F',
+        file.name
+    ];
+    if(!file.isDirectory()){
+        const {size} = await stat(file.name);
+        parts.push(`${size}o`);
+    }
+    console.log(parts.join(' - '));
 }
